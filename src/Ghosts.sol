0c0866698bdc3a93632dfcf8d379c134670f1eb2
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-03-18 15:43:08 -0300

    First commit

---
+++ src/Ghosts.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+contract Ghosts {
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+
+    struct Ghost {
+        bytes32 b;
+        bytes32 a;
+        address target;
+        bytes data;
+    }
+
+    mapping(bytes32 => Ghost) internal _ghosts;
+    EnumerableSet.Bytes32Set internal _keys;
+    bytes32 internal _ghosted;
+
+    modifier ghosted(bytes32 key) {
+        _ghosted = key;
+        _;
+        _ghosted = bytes32(0);
+    }
+
+    function __snapshot(bytes32 key) internal returns (bytes32) {
+        Ghost memory g = _ghosts[key];
+        (bool success, bytes memory returnData) = g.target.call(g.data);
+        Address.verifyCallResult(success, returnData);
+        return bytes32(returnData);
+    }
+
+    function _ghost(bytes32 key, address target, bytes memory data) public {
+        _ghosts[key] = Ghost({target: target, data: data, b: bytes32(0), a: bytes32(0)});
+        _keys.add(key);
+    }
+
+    function _call(address target, bytes memory data) internal {
+        _ghosts[_ghosted].b = __snapshot(_ghosted);
+        (bool success, bytes memory returnData) = target.call(data);
+        Address.verifyCallResult(success, returnData);
+        _ghosts[_ghosted].a = __snapshot(_ghosted);
+    }
+
+    function _beforeUint256(bytes32 key) internal view returns (uint256) {
+        return uint256(_ghosts[key].b);
+    }
+
+    function _afterUint256(bytes32 key) internal view returns (uint256) {
+        return uint256(_ghosts[key].a);
+    }
+}
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,26 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICSENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
+import {Ghosts} from "../src/Ghosts.sol";
 
-contract CounterTest is Test {
+contract CounterTest is Test, Ghosts {
     Counter public counter;
 
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+        _ghost("number", address(counter), abi.encodeCall(counter.number, ()));
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_Increment() public ghosted("number") {
+        _call(address(counter), abi.encodeCall(counter.increment, ()));
+        assertEq(_afterUint256("number"), 1);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testFuzz_SetNumber(uint256 x) public ghosted("number") {
+        _call(address(counter), abi.encodeCall(counter.setNumber, (x)));
+        assertEq(_afterUint256("number"), x);
     }
 }
